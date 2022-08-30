78e308f0504e88abade0db4b87f7f0a40a782d2b
Iulia Dogar <iulia.dogar@p4b.ro>
2022-08-30 17:50:16 +0300

    changes related to Swagger authorisation

--- src/main/java/licenta/utcnforum/server/config/SecurityConfig.java
+++ src/main/java/licenta/utcnforum/server/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/user/login").permitAll()
+                .authorizeRequests().antMatchers("/user/login", "/post", "/post/utcn-news", "/post/category**", "/category").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
